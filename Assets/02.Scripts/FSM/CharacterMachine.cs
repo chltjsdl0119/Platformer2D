800af0b569acf0400139d127761a96e9cf4346c4
Seungwon-Choi <bestwondev@gmail.com>
2025-06-22 13:18:22 +0900

    feat: FSM

---
+++ Assets/02.Scripts/FSM/CharacterMachine.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum State
+{
+    None,
+    Idle,
+    Move,
+    Jump,
+    Fall,
+    Land
+}
+
+public abstract class CharacterMachine : MonoBehaviour
+{
+    // Direction
+    public int direction
+    {
+        get => _direction;
+        set
+        {
+            if (isDirectionChangeable == false)
+            {
+                return;
+            }
+
+            if (_direction == value)
+            {
+                return;
+            }
+
+            if (value > 0)
+            {
+                transform.eulerAngles = Vector3.zero;
+                _direction = DIRECTION_RIGHT;
+            } 
+            else if (value < 0)
+            {
+                transform.eulerAngles = Vector3.up * 180.0f;
+                _direction = DIRECTION_LEFT;
+            }
+            else
+            {
+                throw new System.Exception("[CharacterMachine] Invalid direction value: " + value);
+            }
+        }
+    }
+
+    private int _direction;
+    [HideInInspector] public bool isDirectionChangeable;
+
+    public const int DIRECTION_RIGHT = 1;
+    public const int DIRECTION_LEFT = -1;
+    
+    // Movement
+    public virtual float horizontal { get; set; }
+    public float speed;
+    [HideInInspector] public Vector2 move;
+    [HideInInspector] public bool isMovable;
+    private Rigidbody2D _rigidbody;
+    
+    public State current;
+    private Dictionary<State, IWorkflow<State>> _states;
+    
+    public void Initialize(IEnumerable<KeyValuePair<State, IWorkflow<State>>> copy)
+    {
+        _states = new Dictionary<State, IWorkflow<State>>(copy);
+        current = copy.First().Key;
+    }
+
+    public bool ChangeState(State newState)
+    {
+        if (newState == current)
+        {
+            return false;
+        }
+
+        current = newState;
+        _states[newState].Reset();
+        return true;
+    }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _direction = DIRECTION_RIGHT;
+    }
+
+    private void Update()
+    {
+        ChangeState(_states[current].MoveNext());
+
+        if (isMovable)
+        {
+            move = new Vector2(horizontal * speed, 0.0f);
+        }
+
+        if (isDirectionChangeable && Mathf.Abs(horizontal) > 0.0f)
+        {
+            direction = horizontal < 0.0f ? DIRECTION_LEFT : DIRECTION_RIGHT;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.position += move * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/02.Scripts/FSM/CharacterStateWorkflowsDataSheet.cs
@@ -0,0 +1,112 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CharacterStateWorkflowsDataSheet
+{
+    public abstract class WorkflowBase : IWorkflow<State>
+    {
+        public abstract State ID { get; }
+
+        public int Current => current;
+        protected int current;
+
+        protected CharacterMachine machine;
+        protected Transform transform;
+        protected Rigidbody2D rigidbody;
+        protected CapsuleCollider2D collider;
+
+        public WorkflowBase(CharacterMachine machine)
+        {
+            this.machine = machine;
+            this.transform = machine.transform;
+            this.rigidbody = machine.GetComponent<Rigidbody2D>();
+            this.collider = machine.GetComponent<CapsuleCollider2D>();
+        }
+
+        public abstract State MoveNext();
+
+        public void Reset()
+        {
+            current = 0;
+        }
+    }
+
+    public class Idle : WorkflowBase
+    {
+        public override State ID => State.Idle;
+
+        public Idle(CharacterMachine machine) : base(machine)
+        {
+        }
+
+        public override State MoveNext()
+        {
+            State next = ID;
+
+            switch (current)
+            {
+                case 0:
+                {
+                    machine.isDirectionChangeable = true;
+                    machine.isMovable = true;
+                    current++;
+                }
+                    break;
+                default:
+                {
+                    if (Mathf.Abs(machine.horizontal) > 0.0f)
+                    {
+                        next = State.Move;
+                    }
+                }
+                    break;
+            }
+
+            return next;
+        }
+    }
+
+    public class Move : WorkflowBase
+    {
+        public override State ID => State.Idle;
+
+        public Move(CharacterMachine machine) : base(machine)
+        {
+        }
+
+        public override State MoveNext()
+        {
+            State next = ID;
+
+            switch (current)
+            {
+                case 0:
+                {
+                    machine.isDirectionChangeable = true;
+                    machine.isMovable = true;
+                    current++;
+                }
+                    break;
+                default:
+                {
+                    if (machine.horizontal == 0.0f)
+                    {
+                        next = State.Idle;
+                    }
+                }
+                    break;
+            }
+
+            return next;
+        }
+    }
+
+    public static IEnumerable<KeyValuePair<State, IWorkflow<State>>> GetWorkflowsForPlayer(CharacterMachine machine)
+    {
+        return new Dictionary<State, IWorkflow<State>>()
+        {
+            { State.Idle, new Idle(machine) },
+            { State.Move, new Move(machine) },
+        };
+    }
+}
